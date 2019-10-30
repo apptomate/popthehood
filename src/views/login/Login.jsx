d5465f17cacdc9e004d4c572e874aad9f607b133
varunsapp <varuns@apptomate.co>
2019-10-30 16:23:30 +0530

    crash test function

--- src/views/login/Login.jsx
+++ src/views/login/Login.jsx
@@ -119,6 +119,7 @@ class Login extends React.Component {
                   >
                     Sign in
                   </Button>
+                  <button onClick={methodDoesNotExist}>Break the world</button>;
                 </div>
               </Form>
             </CardBody>
