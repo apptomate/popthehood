98c3a0e252f19a17222568dd5c2db1e7ab599f64
kar-thi <karthim@apptomate.co>
2019-11-01 01:26:11 +0530

    Last design allignment-1

--- src/components/Footers/AdminFooter.jsx
+++ src/components/Footers/AdminFooter.jsx
@@ -8,7 +8,7 @@ class Footer extends React.Component {
     return (
       <footer className="footer">
         <Row className="align-items-center justify-content-xl-between">
-          <Col xl="6">
+          <Col xl="12">
             <div className="copyright text-center text-xl-left text-muted">
               © 2019
               <a
--- src/views/common/PageNotFound.jsx
+++ src/views/common/PageNotFound.jsx
@@ -1,19 +1,35 @@
 import React, { Component, Fragment } from 'react';
+import { Card, Container, Row } from 'reactstrap';
+import UserHeader from 'components/Headers/UserHeader.jsx';
 
 class PageNotFound extends Component {
   render() {
     return (
       <Fragment>
-        <div>
-          <div id="notfound">
-            <div className="notfound">
-              <div className="notfound-404">
-                <h1>Oops!</h1>
-              </div>
-              <h2>404 - Page not found</h2>
+        <UserHeader />
+        <Container className="mt--7">
+          <Row>
+            <div className="col">
+              <Card className="shadow p-4">
+                <div id="notfound">
+                  <div className="notfound">
+                    <div className="notfound-404">
+                      <h3>Oops! Page not found</h3>
+                      <h1>
+                        <span>4</span>
+                        <span>0</span>
+                        <span>4</span>
+                      </h1>
+                    </div>
+                    <h2>
+                      we are sorry, but the page you requested was not found
+                    </h2>
+                  </div>
+                </div>
+              </Card>
             </div>
-          </div>
-        </div>
+          </Row>
+        </Container>
       </Fragment>
     );
   }
