52d639e3dc93a8ca1da6424007a3a7d27b55d13e
SekarGaneshAppDev <ganeswarana@apptomate.co>
2019-11-18 17:56:03 +0530

    Users,Teams and Match list displayed

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
-import * as Sentry from '@sentry/browser';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import * as Sentry from "@sentry/browser";
 
-import 'assets/vendor/nucleo/css/nucleo.css';
-import 'assets/vendor/@fortawesome/fontawesome-free/css/all.min.css';
-import 'assets/scss/popTheHood-dashboard-react.scss';
+import "assets/vendor/nucleo/css/nucleo.css";
+import "assets/vendor/@fortawesome/fontawesome-free/css/all.min.css";
+import "assets/scss/popTheHood-dashboard-react.scss";
 
-import AdminLayout from 'layouts/Admin.jsx';
-import AuthLayout from 'layouts/Auth.jsx';
+import AdminLayout from "layouts/Admin.jsx";
+import AuthLayout from "layouts/Auth.jsx";
 
-import { Provider } from 'react-redux';
-import configureStore from '../src/redux/store/ConfigureStore.jsx';
+import { Provider } from "react-redux";
+import configureStore from "../src/redux/store/ConfigureStore.jsx";
 
 Sentry.init({
-  dsn: 'https://b23620d2d0a448f697313eb6b290e6cc@sentry.io/1801912'
+  dsn: "https://b23620d2d0a448f697313eb6b290e6cc@sentry.io/1801912"
 });
 
 const store = configureStore();
@@ -23,11 +23,11 @@ ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <Switch>
-        <Route path="/auth" render={props => <AuthLayout {...props} />} />
-        <Route path="/admin" render={props => <AdminLayout {...props} />} />
-        <Redirect from="/" to="/auth/login" />
+        <Route path='/auth' render={props => <AuthLayout {...props} />} />
+        <Route path='/admin' render={props => <AdminLayout {...props} />} />
+        <Redirect from='/' to='/auth/login' />
       </Switch>
     </BrowserRouter>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
